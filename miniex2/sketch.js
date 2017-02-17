ba3d8121ca7cc4a21c89135475cfd91fae1f79e2
a9neh <ani_hansen@hotmail.com>
2017-02-17 11:12:27 +0100

    Add files via upload

---
+++ miniex2/sketch.js
@@ -0,0 +1,25 @@
+function setup() {
+  createCanvas(800, 800);
+  background(255, 192, 203);
+}
+
+function draw() {
+  fill(250, 250, 250);
+  stroke(250, 250, 250);
+  rect(250, 300, 250, 300);
+  fill(250, 250, 250);
+  rect(250, 160, 55, 140);
+  fill(250, 250, 250);
+  rect(445, 160, 55, 140)
+  fill(220, 20, 60);
+  strokeWeight(2)
+  stroke(220, 20, 60);
+  textSize(36);
+  text("TAHNK YOU", 265, 480);
+  fill(250, 250, 250);
+  textSize(36);
+  text("TAHNK YOU", 265, 520);
+  text("TAHNK YOU", 265, 560);
+  text("TAHNK YOU", 265, 440);
+  text("TAHNK YOU", 265, 400);
+}
